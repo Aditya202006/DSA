5898d36e63c89b902f3b7b7fcb43ac3030f2d294
SAI NAGENDRA <adityakanchana@gmail.com>
2025-06-21 11:45:40 +0530

    287. Find the Duplicate Number.java

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,35 @@
+//  287. Find the Duplicate Number [leetcode]
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        return cycle(nums);
+    }
+    static int cycle(int[] nums){
+        int i=0;
+        while(i<nums.length){
+            int correctindex = nums[i] - 1;
+            if(nums[i] != nums[correctindex]){
+                swap(nums,i,correctindex);
+            }else{
+                i++;
+            }
+        }
+        int res=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] != j + 1){
+                res = nums[j];
+            }
+        }
+        return res;
+    }
+    static void swap(int[] nums,int a,int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+}
+
+//OUTPUT
+
+// [5,6]
