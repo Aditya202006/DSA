178e459a1dfb04d39a5ebedd2e9cedb3c2d8c763
SAI NAGENDRA <adityakanchana@gmail.com>
2025-06-24 11:51:47 +0530

    Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -86,3 +86,67 @@ class Sample {
         }
     }
 }
+
+
+//OUTPUT
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+
+import java.util.*;
+class Sample {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for(int i=1;i<=2*n - 1;i++){
+            int col = i > n ?  2 * n - i : i;
+            for(int j=1;j<=col;j++){
+                System.out.print("* ");
+            }
+            System.out.println();;
+        }
+    }
+}
+
+
+//OUTPUT
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    *
+
+import java.util.*;
+class Sample {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for(int i=1;i<=2*n - 1;i++){
+            int col = i > n ?  2 * n - i : i;
+            int spaces = n - col;
+            for(int s=0;s<spaces;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=col;j++){
+                System.out.print("* ");
+            }
+            System.out.println();;
+        }
+    }
+}
+
+
+// OUTPUT
