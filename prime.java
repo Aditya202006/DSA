7bdb1ef2f59a928e3e7d289f5c621325de7f6a47
SAI NAGENDRA <adityakanchana@gmail.com>
2025-08-02 17:12:31 +0530

    Prime.java

---
+++ prime.java
@@ -0,0 +1,23 @@
+// Prime number
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isprime(n));
+    }
+    public static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        int c = 2;
+        while(c*c <= n){
+            if(n %c ==0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+}
