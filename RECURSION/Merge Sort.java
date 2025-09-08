b5c76e1a06be1156a248ae88906f5733e2f0736b
SAI NAGENDRA <adityakanchana@gmail.com>
2025-09-08 18:41:30 +0530

    Merge Sort.java

---
+++ RECURSION/Merge Sort.java
@@ -0,0 +1,46 @@
+// Merge Sort 
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        int [] arr={3,1,6,8,9,2};
+        arr = mergesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] mergesort(int[] arr){
+        if(arr.length == 1)
+            return arr;
+        int mid=arr.length/2;
+        int [] left = mergesort(Arrays.copyOfRange(arr,0,mid));
+        int [] right = mergesort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+    }
+    public static int[] merge(int[] left,int[] right){
+        int [] ans = new int[left.length+right.length];
+        int i=0,j=0,k=0;
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                ans[k]=left[i];
+                i++;
+            }
+            else{
+                ans[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<left.length){
+            ans[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j<right.length){
+            ans[k] = right[j];
+            j++;
+            k++;
+        }
+        return ans;
+    }
+}
+
+// [1, 2, 3, 6, 8, 9]
